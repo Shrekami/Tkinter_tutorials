31f10fe52a39c8eb74d1d90a540e679b4b234942
zionkex <gicubruja@gmail.com>
2025-04-28 10:06:54 +0300

    вніс зміни подивись коментарі

--- calculator.py
+++ calculator.py
@@ -4,7 +4,6 @@ app=Tk()
 app.geometry("500x625+400+50")
 app["bg"]="#4eb4a1"
 app.title("Calculator")
-app.resizable(False,False)
 
 zeronul=0
 lblx=0
@@ -25,8 +24,7 @@ def q(number):
 
 top=Label(text="This is a calculator that can count",width=50,height=10,bg="#4e92b4")
 # top.place(x=80,y=50)
-top.grid(row=0,column=0,columnspan=6,rowspan=2,pady=(5, 15))
-
+top.grid(row=0,column=0,columnspan=4,rowspan=2,padx=5,pady=5,sticky="nsew")
 def btrv():
     global znak,num,num2,lblx,lbly,nul,zeronul
     if znak =="+":
@@ -68,52 +66,61 @@ def clear():
     nul = 0
 
 button1=Button(text="1",command=lambda :q(1),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button1.grid(row=2,column=0,padx=10,pady=10,ipadx=10)
+button1.grid(row=2,column=0,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button2=Button(text="2",command=lambda :q(2),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button2.grid(row=2,column=1,padx=10,pady=10,ipadx=10)
+button2.grid(row=2,column=1,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button3=Button(text="3",command=lambda :q(3),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button3.grid(row=2,column=2,padx=10,pady=10,ipadx=10)
+button3.grid(row=2,column=2,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button4=Button(text="4",command=lambda :q(4),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button4.grid(row=2,column=3,padx=10,pady=10,ipadx=10)
+button4.grid(row=2,column=3,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button5=Button(text="5",command=lambda :q(5),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button5.grid(row=3,column=0,padx=10,pady=10,ipadx=10)
+button5.grid(row=3,column=0,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button6=Button(text="6",command=lambda :q(6),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button6.grid(row=3,column=1,padx=10,pady=10,ipadx=10)
+button6.grid(row=3,column=1,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button7=Button(text="7",command=lambda :q(7),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button7.grid(row=3,column=2,padx=10,pady=10,ipadx=10)
+button7.grid(row=3,column=2,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button8=Button(text="8",command=lambda :q(8),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button8.grid(row=3,column=3,padx=10,pady=10,ipadx=10)
+button8.grid(row=3,column=3,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button9=Button(text="9",command=lambda :q(9),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button9.grid(row=4,column=0,padx=10,pady=10,ipadx=10)
+button9.grid(row=4,column=0,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button10=Button(text="0",command=lambda :q(0),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button10.grid(row=4,column=1,padx=10,pady=10,ipadx=10)
+button10.grid(row=4,column=1,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button11=Button(text="+",command=lambda:btnznak("+"),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button11.grid(row=4,column=2,padx=10,pady=10,ipadx=10)
+button11.grid(row=4,column=2,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button12=Button(text="-",command=lambda:btnznak("-"),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button12.grid(row=4,column=3,padx=10,pady=10,ipadx=10)
+button12.grid(row=4,column=3,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button13=Button(text="*",command=lambda:btnznak("*"),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button13.grid(row=5,column=0,padx=10,pady=10,ipadx=10)
+button13.grid(row=5,column=0,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button14=Button(text="/",command=lambda:btnznak("/"),width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button14.grid(row=5,column=1,padx=10,pady=10,ipadx=10)
+button14.grid(row=5,column=1,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button15=Button(text="=",command=btrv,width=5,height=2,bg=Config.button_equal_bg,fg=Config.button_fg,font=Config.button_font)
-button15.grid(row=5,column=2,padx=10,pady=10,ipadx=10)
+button15.grid(row=5,column=2,padx=10,pady=10,ipadx=10,sticky="nsew")
 
 button16=Button(text="clear",command=clear,width=5,height=2,bg=Config.button_bg,fg=Config.button_fg,font=Config.button_font)
-button16.grid(row=5,column=3,padx=10,pady=10,ipadx=10)
+button16.grid(row=5,column=3,padx=10,pady=10,ipadx=10,sticky="nsew")
+
+for col in range(4):
+    # робить колонки однаковими по ширині
+    app.grid_columnconfigure(col, weight=1)
+
+for row in range(5):
+    # робить рядки однаковими по висоті
+    app.grid_rowconfigure(row, weight=1)
+
 app.mainloop()
 
-# Я зробив домашнє завдання, але синя штука не рухалася разом з цифрами. Як мені зробити так, щоб синя смужка стала посередині?
+# sticky="nsew" - розтягує кнопку в всі сторони.враховує відстань між кнопками padx,pady
