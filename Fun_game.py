7a347248f3fbf8ea2aac57060f92ec6696bf62af
Shrekami <shrekad0mp@gmail.com>
2025-05-06 21:11:15 -0700

    I just have a question how o you find out if a button is pressed?

---
+++ Fun_game.py
@@ -0,0 +1,26 @@
+import random
+from tkinter import *
+app=Tk()
+app.geometry("600x500+400+50")
+app.title("Robot")
+
+computer_choice=Label(text="computer_choice",width=12, height=14)
+computer_choice.pack()
+
+winner=Label(text="Winner")
+winner.pack()
+
+def click():
+    print("hi")
+
+rock=Button(text="Rock",width=6,height=1,fg="#000000")
+rock.pack(padx=10,fill="x",expand=True,side="left")
+
+paper=Button(text="Paper",width=6,height=1,fg="#000000")
+paper.pack(padx=10,side="left",fill="x",expand=True)
+
+scissors=Button(text="Scissors",width=6,height=1,fg="#000000")
+scissors.pack(padx=10,side="left",fill="x",expand=True)
+
+app.mainloop()
+#if you press a button it needs to show you computer choice, for that use random.choice([rock,paper,scissor])
