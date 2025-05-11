c2cca94ecfd5b8d29b3c50df22070f1bea29e571
Shrekami <shrekad0mp@gmail.com>
2025-05-11 09:17:03 -0700

    Rock, paper, scissors, version 1

--- Fun_game.py
+++ Fun_game.py
@@ -4,22 +4,55 @@ app=Tk()
 app.geometry("600x500+400+50")
 app.title("Robot")
 
-computer_choice=Label(text="computer_choice",width=12, height=14)
+computer_choice=Label(text="computer_choice",width=32, height=14)
 computer_choice.pack()
-def get_computer_choice():
-    random.choice([rock, paper, scissors])
-    computer_choice['text']=Label(computer_choice)
+def get_computer_choice(my_choice):
+    global computer_choice
+    nlo=random.choice(["rock", "paper", "scissors"])
+    computer_choice['text']=f"Computer chose {nlo}"
+    print("Computer chose",nlo)
+    win(my_choice=my_choice,machine_choice=nlo)
+
 winner=Label(text="Winner")
 winner.pack()
 
+def king(human):
+    global winner
+    winner["text"]=f"The winner is {human}"
+
+def win(my_choice,machine_choice):
+    if my_choice==machine_choice:
+        human="nobody"
+    if my_choice=="scissors":
+        if machine_choice=="rock":
+            human="computer"
+        elif machine_choice=="paper":
+            human="human"
+
+    if my_choice=="rock":
+        if machine_choice=="scissors":
+           human="human"
+        elif machine_choice=="paper":
+            human="computer"
+
+    if my_choice=="paper":
+        if machine_choice=="rock":
+            human="human"
+        elif machine_choice=="scissors":
+            human="computer"
+    king(human)
+
 def click():
     print("You chose rock")
+    get_computer_choice(my_choice="rock")
 
 def clank():
     print("You chose paper")
+    get_computer_choice(my_choice="paper")
 
 def clonk():
     print("You chose scissors")
+    get_computer_choice(my_choice="scissors")
 
 rock=Button(text="Rock",command=click,width=6,height=1,fg="#000000",)
 rock.pack(padx=10,fill="x",expand=True,side="left")
