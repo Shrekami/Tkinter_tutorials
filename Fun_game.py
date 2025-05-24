23d26adf944c638563a877e84ae0ebed01019416
Shrekami <shrekad0mp@gmail.com>
2025-05-24 08:57:43 -0700

    Everything

--- Fun_game.py
+++ Fun_game.py
@@ -5,7 +5,10 @@ app.geometry("600x500+400+50")
 app.title("Robot")
 app["bg"]="#527bf5"
 
-computer_choice=Label(text="computer_choice",width=62, height=6,bg="#52f5e8")
+lives=2
+points=0
+
+computer_choice=Label(text="computer_choice",width=100, height=6,bg="#52f5e8",font="12")
 computer_choice.pack()
 
 def get_computer_choice(my_choice):
@@ -15,13 +18,15 @@ def get_computer_choice(my_choice):
     print("Computer chose",nlo)
     win(my_choice=my_choice,machine_choice=nlo)
 
-winner=Label(text="Winner",width=40,bg="#5ff3d4")
+winner=Label(text="Winner",width=40,bg="#5ff3d4",font="24")
 winner.pack(pady=70)
 
 def king(human):
     global winner
-    winner["text"]=f"The winner is {human}"
+    name=input("What is going to be your username? ")
+    winner["text"]=f"{name} has {points} points and {lives} lives"
     color(human)
+
 def color(human):
     if human=="nobody":
         winner["bg"]="#f1a14c"
@@ -31,57 +36,92 @@ def color(human):
         winner["bg"]="#189b4b"
 
 def win(my_choice,machine_choice):
+    global lives
+    global points
     if my_choice==machine_choice:
         human="nobody"
     if my_choice=="scissors":
         if machine_choice=="rock":
             human="computer"
+
+            if lives==2:
+                lives -= 1
+                print("You now have", lives ,"life")
+
+            elif lives==1:
+                lives -= 1
+                print("You lost since you have", lives ,"lives")
+                rock.config(state="disabled")
+                paper.config(state="disabled")
+                scissors.config(state="disabled")
+
         elif machine_choice=="paper":
             human="human"
+            points+=100
+            print("You now have", points, "points")
 
     if my_choice=="rock":
         if machine_choice=="scissors":
            human="human"
+           points+=200
+           print("You now have", points, "points")
+
         elif machine_choice=="paper":
             human="computer"
+
+            if lives == 2:
+                lives -= 1
+                print("You now have", lives, "life")
+
+            elif lives == 1:
+                lives -= 1
+                print("You lost since you have", lives, "lives")
+                rock.config(state="disabled")
+                paper.config(state="disabled")
+                scissors.config(state="disabled")
 
     if my_choice=="paper":
         if machine_choice=="rock":
             human="human"
+            points += 300
+            print("You now have", points, "points")
+
         elif machine_choice=="scissors":
             human="computer"
+
+            if lives == 2:
+                lives -= 1
+                print("You now have", lives, "life")
+
+            elif lives == 1:
+                lives -= 1
+                print("You lost since you have", lives, "lives")
+                rock.config(state="disabled")
+                paper.config(state="disabled")
+                scissors.config(state="disabled")
     king(human)
 
 def yay(chosen):
     rock.config(bd=5, relief="flat")
     paper.config(bd=5, relief="flat")
     scissors.config(bd=5, relief="flat")
-
     chosen.config(bd=5,relief="raised")
 
-def click():
-    print("You chose rock")
-    yay(rock)
-    get_computer_choice(my_choice="rock",)
+def click_button(chosen):
+    print(f"You chose",chosen ["text"])
+    yay(chosen)
+    get_computer_choice(my_choice=chosen ["text"].lower())
 
-def clank():
-    print("You chose paper")
-    yay(paper)
-    get_computer_choice(my_choice="paper")
+rock=Button(text="Rock",command=lambda:click_button(rock),width=5,height=1,bg="#f03521")
+rock.pack(pady=50,padx=10,fill="x",expand=True,side="left")
 
-def clonk():
-    print("You chose scissors")
-    yay(scissors)
-    get_computer_choice(my_choice="scissors")
-
-rock=Button(text="Rock",command=click,width=6,height=1,bg="#f03521",)
-rock.pack(pady=100,padx=10,fill="x",expand=True,side="left")
-
-paper=Button(text="Paper",command=clank,width=6,height=1,bg="#7af35f")
+paper=Button(text="Paper",command=lambda:click_button(paper),width=5,height=1,bg="#7af35f")
 paper.pack(padx=10,side="left",fill="x",expand=True)
 
-scissors=Button(text="Scissors",command=clonk,width=6,height=1,bg="#5f94f3")
+scissors=Button(text="Scissors",command=lambda:click_button(scissors),width=5,height=1,bg="#5f94f3")
 scissors.pack(padx=10,side="left",fill="x",expand=True)
 
+# app.destroy()
+# result=Tk()
 app.mainloop()
-# make a frame to each button that you press
+# eact time you win earn 100 more points, you have two lives, make top player leader,add input for sign in
--- dictionary_learning.py
+++ dictionary_learning.py
@@ -9,9 +9,79 @@
 # for k1,v1 in family.items():
 #     print(f"My {k1}s' name is {v1}")
 
-city="Seattle is a vibrant city in the Pacific Northwest, known for its iconic Space Needle, beautiful waterfront, and as the birthplace of tech giants like Microsoft and Amazon."
-myd={"a":0,"b":0,"c":0,"d":0,"e":0,"f":0,"g":0,"h":0,"i":0,"j":0,"k":0,"l":0,"m":0,"n":0,"o":0,"p":0,"q":0,"r":0,"s":0,"t":0,"u":0,"v":0,"w":0,"x":0,"y":0,"z":0,}
-for k1,v1 in myd():
-    print(f"{k1} equals {v1}")
+# city="Seattle is a vibrant city in the Pacific Northwest, known for its iconic Space Needle, beautiful waterfront, and as the birthplace of tech giants like Microsoft and Amazon."
+# myd={}
+# for i in city.lower():
+#     if i in myd.keys():
+#         myd[i]+=1
+#     else:
+#         myd[i]=1
+#         print(myd)
+# myd=dict(sorted(myd.items()))
+# for letter,count in myd.items():
+#     print(letter,"appears", count,"times")
+# myd['Seattle']=1111
+# myd["seattle"]=2222
+# print(myd)
 
-# Count each letter
+# eat={"cucumber":3,"tomatoes":10,"banana":1,"watermelon":2}
+#
+# def add():
+#     bor=input("What is the name of the new product, you would like to add? ")
+#     fer=float(input("What will be the cost of your product? "))
+#     eat[bor]=fer
+#     print(eat)
+#
+# def refresh():
+#     limb=input("Which product will you update cost? ")
+#     romp=int(input("Type a new price "))
+#     eat[limb]=romp
+#     print(eat)
+#
+# def delete():
+#   limp=input("Which product will you delete? ")
+#   del eat[limp]
+# #
+# def pick():
+#     for product,count in eat.items():
+#         print(product,"with price of",count,"dollars")
+#
+# def count():
+#     fomp = input("Which product will you get count of? ")
+#     print(eat[fomp],'dollars')
+#
+# while True:
+#     print("1 = Add new product")
+#     print("2 = update_product_cost")
+#     print("3 = Delete product")
+#     print("4 = Get my products")
+#     print("5 = Get product counts")
+#     print("6 = Exit")
+#     lomp=int(input("Choose your function "))
+#     if lomp==1:
+#         add()
+#
+#     elif lomp==2:
+#         refresh()
+#
+#     elif lomp==3:
+#         delete()
+#
+#     elif lomp==4:
+#         pick()
+#
+#     elif lomp==5:
+#         count()
+#
+#     elif lomp==6:
+#         break
+
+
+import json
+with open("donkey_nice.json","r")as file:
+    data=json.load(file)
+    new_user={'name':"bob","age": 13,"score":100,"color":"hispanic"}
+    data.append(new_user)
+    print(data)
+with open("donkey_nice.json","w+t")as file:
+    json.dump(data,file,indent=4)
