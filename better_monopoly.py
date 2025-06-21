768cbfcedabf33a8ccff68db6d4dd720bf0650de
Shrekami <shrekad0mp@gmail.com>
2025-06-21 10:02:11 -0700

    There is a problem when you want to play the app.

--- better_monopoly.py
+++ better_monopoly.py
@@ -1,24 +1,23 @@
 from tkinter import *
+from winning_window import noice
+from winning_window import bad
 import json
 
 
 with open ('data_monopoly.json',"r") as file:
     data=json.load(file)
-app=Tk()
-app.geometry('800x800')
 
-lives=2
+lives=1
 money=0
 number=0
 print(data[number])
 
-def altogether():
-    global lives,money,number,gaming,gaming1,gaming2,gaming3
-    donkey = Toplevel()
-
-def update_question(gaming,gaming1,gaming2,gaming3):
-    global number
+def update_question():
+    global gaming, gaming1, gaming2, gaming3
+    global lives
     global money
+    global number
+    global money,account_money,showing,app
     number+=1
     if number==2:
         money+=0
@@ -52,10 +51,11 @@ def update_question(gaming,gaming1,gaming2,gaming3):
         money+=200000
         account_money['text'] = money
 
-    elif number==10:
+    if number==10:
         money+=700000
         account_money['text'] = money
         app.withdraw()
+        noice(money)
 
     else:
         showing["text"]=data[number]["question"]
@@ -70,7 +70,7 @@ def next_question(answer):
     global money
     if answer==data[number]["correct_answer"]:
         print("Nice! You got the right answer")
-        update_question(gaming,gaming1,gaming2,gaming3)
+        update_question()
     else:
         print("Wrong answer")
         lives-=1
@@ -78,6 +78,7 @@ def next_question(answer):
         account_money['text'] = money
         if lives==0:
             app.withdraw()
+            bad(lives)
 
 def half_help():
     pass
@@ -87,34 +88,37 @@ def friend_help():
 
 def audience_help():
     pass
+def start_game(new_game_window):
+    new_game_window.destroy()
+    app = Tk()
+    app.geometry('800x800')
+    gaming=Button(text=data[number]["choices"][0],command=lambda:next_question(0),width=53, height=5,bg='blue')
+    gaming.grid(row=9,column=0,padx=15,pady=8,columnspan=2)
 
-gaming=Button(text=data[number]["choices"][0],command=lambda:next_question(0),width=53, height=5,bg='blue')
-gaming.grid(row=9,column=0,padx=15,pady=8,columnspan=2)
+    gaming1=Button(text=data[number]["choices"][1],command=lambda:next_question(1),width=53, height=5,bg='blue')
+    gaming1.grid(row=10,column=0,padx=15,pady=8,columnspan=2)
 
-gaming1=Button(text=data[number]["choices"][1],command=lambda:next_question(1),width=53, height=5,bg='blue')
-gaming1.grid(row=10,column=0,padx=15,pady=8,columnspan=2)
+    gaming2=Button(text=data[number]["choices"][2],command=lambda:next_question(2),width=53, height=5,bg='blue')
+    gaming2.grid(row=9,column=2,padx=15,pady=8,columnspan=2)
 
-gaming2=Button(text=data[number]["choices"][2],command=lambda:next_question(2),width=53, height=5,bg='blue')
-gaming2.grid(row=9,column=2,padx=15,pady=8,columnspan=2)
+    gaming3=Button(text=data[number]["choices"][3],command=lambda:next_question(3),width=53, height=5,bg='blue')
+    gaming3.grid(row=10,column=2,padx=2,pady=8,columnspan=2)
 
-gaming3=Button(text=data[number]["choices"][3],command=lambda:next_question(3),width=53, height=5,bg='blue')
-gaming3.grid(row=10,column=2,padx=2,pady=8,columnspan=2)
+    some_help=Button(text="?",command=half_help,width=15, height=2,bg='green')
+    some_help.grid(row=5,column=0,padx=20,pady=115)
 
-some_help=Button(text="?",command=half_help,width=15, height=2,bg='green')
-some_help.grid(row=5,column=0,padx=20,pady=115)
+    friend=Button(text="?",command=friend_help,width=15, height=2,bg='green')
+    friend.grid(row=5,column=1,padx=20,pady=115,columnspan=2)
 
-friend=Button(text="?",command=friend_help,width=15, height=2,bg='green')
-friend.grid(row=5,column=1,padx=20,pady=115,columnspan=2)
+    audience=Button(text="?",command=audience_help,width=15, height=2,bg='green')
+    audience.grid(row=5,column=3,padx=20,pady=115)
 
-audience=Button(text="?",command=audience_help,width=15, height=2,bg='green')
-audience.grid(row=5,column=3,padx=20,pady=115)
+    showing=Label(text=data[number]["question"],width=108, height=5,bg='aqua')
+    showing.grid(row=6,column=0,padx=0,pady=12,columnspan=6)
 
-showing=Label(text=data[number]["question"],width=108, height=5,bg='aqua')
-showing.grid(row=6,column=0,padx=0,pady=12,columnspan=6)
-
-account_money=Label(text="?",width=18, height=3,bg='blue')
-account_money.grid(row=1,column=3,padx=0,pady=0)
-# You have to check other way how to check for the win:Good
-# Add money in the top label:Worked but doesn't work now because of the switching windows
-# Winning and losing window: Confused
-app.mainloop()
+    account_money=Label(text="?",width=18, height=3,bg='blue')
+    account_money.grid(row=1,column=3,padx=0,pady=0)
+    # You have to check other way how to check for the win:Good
+    # Add money in the top label:Worked but doesn't work now because of the switching windows
+    # Winning and losing window: Confused
+    app.mainloop()
