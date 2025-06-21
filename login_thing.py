a28276e6680503ec00f04c6c6069e330d9cdd660
zionkex <gicubruja@gmail.com>
2025-06-21 22:27:26 +0300

    fix errors for game window

--- login_thing.py
+++ login_thing.py
@@ -1,14 +1,15 @@
 from tkinter import *
+
+from better_monopoly import new_game
+
 app=Tk()
 app.geometry('800x800')
 
-def new_game():
-    pass
 
 def exit():
     pass
 
-game=Button(text='New game',command=new_game,width=115, height=25,bg='blue')
+game=Button(text='New game',command=lambda: new_game(app),width=115, height=25,bg='blue')
 game.grid(row=1,column=1)
 
 exiting=Button(text='Exit',command=exit,width=115, height=25,bg='yellow')
