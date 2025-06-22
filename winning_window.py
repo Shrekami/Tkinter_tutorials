fe0211e96c07c238b1be18597347ade07843e5dc
Shrekami <shrekad0mp@gmail.com>
2025-06-21 20:47:45 -0700

    There is a problem because you can't lose it makes some notification about nonetype object has no attribute "withdraw" after that notification the question and the choices don't change.

--- winning_window.py
+++ winning_window.py
@@ -1,9 +1,14 @@
 from tkinter import *
-from better_monopoly import money
-app=Tk()
-app.geometry("400x450+200+225")
+def noice(money):
+    app = Tk()
+    app.geometry("800x450+200+225")
+    look = Label(app,text=f"You won {money} money", width=75, height=30, bg='blue')
+    look.grid(row=1, column=3, padx=0, pady=0)
+    app.mainloop()
 
-def noicee():
-
-
-app.mainloop()
+def bad(lives):
+    app = Tk()
+    app.geometry("400x450+200+225")
+    fiasco = Label(app, text=f"You lost because you had {lives} lives.", width=75, height=30, bg='blue')
+    fiasco.grid(row=1, column=3, padx=0, pady=0)
+    app.mainloop()
