6fceccf3dc0e664ee68f9a3e57e546f202d50c6f
Shrekami <shrekad0mp@gmail.com>
2025-07-16 11:00:40 -0700

    Correct version before it is deleted

--- discriminant.py
+++ discriminant.py
@@ -10,14 +10,12 @@ def discriminant():
     discriminants=b ** 2 - 4 * a * c
     global monkey
     global king_kong
-
-
     monkey['text']=("D=b^2-4ac=" #dont touch
-                    f"{b**2}{f'+{a*c*-4}' if a*c*-4>0 else a*c*-4 }={discriminants}\n " # change this
+                    f"{b**2}{f'+{a*c*-4}' if a*c*-4>0 else a*c*-4 }={discriminants}\n" # change this
                             "x1=(-b+sqrt(d))/2a\n" #dont touch sqrt is d**(1/2)
-                            f"x1={(-b+discriminants**(1/2))/2*a} if {discriminants>0} else {(+b+discriminants**(1/2))/2*a} \n"# change this
+                            f"x1={(-b+discriminants**(1/2))/2*a if discriminants>0 else "No more doing that"}\n"# change this
                             "x2=(-b -sqrt(d))/2a\n" #dont touch
-                            f"x2={(-b-discriminants**(1/2))/2*} if {discriminants>0} else you can't do that\n")# change this return calculating if discriminants>0 also 18 row also change the + - thingy
+                            f"x2={(-b-discriminants**(1/2))/2 if discriminants>0 else "No more doing that"}")# change this return calculating if discriminants>0 also 18 row also change the + - thingy
     monkey.pack()
     explain['text']=''
     king_kong['text']=''
@@ -26,17 +24,24 @@ explain=None
 king_kong=None
 monkey=None
 a=1
-b=-4
+b=6
 c=5
+
+def randomizer():
+    for x1 in range (-100,100):
+        for x2 in range(-100,100):
+            if x1+x2 == -b and x1*x2 ==c:
+                return x1,x2
+
 def discriminantiv2():
     global explain
     global monkey
     global king_kong
     monkey['text'] = (f"x1*x2=c\n"#dont touch
                       f"x1+x2=-b\n"#dont touch
-                      f"x1*x2=c\n if {c>0} else you can't solve this"# change -5
-                      f"x1+x2=b\n if {b>0} else you cant solve this"# change -4
-                      f"x1=c x2=a\n") # change this
+                      f"x1*x2={c}\n"# change -5
+                      f"x1+x2={-b}\n"# change -4
+                      f"x1={x1} x2={x2}\n") # change this
     explain['text']=''
 
 def theory():
@@ -54,7 +59,7 @@ def theory():
     if b==1 or b==-1:
         b_text = b_text.replace("1", "")
 
-    squared=Label(text=f'{a_text}x^2+{b_ext}x{f"+{c}"if c>0 else c}')
+    squared=Label(text=f'{a_text}x^2{f"+{b_text}" if b>0 else b_text}x{f"+{c}"if c>0 else c}=0')
     squared.pack()
 
     vieta_d=Radiobutton(command=discriminant,text="Discriminant",variable=group,value=1)
