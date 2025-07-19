099e83650344fa4da7996e79e232a527fa9adbb5
Shrekami <shrekad0mp@gmail.com>
2025-07-18 21:24:13 -0700

    I found a bug in the code, when I was doing the colors I wanted to check how it looks, and it let me run the app but when I clicked on the second radiobutton there was a mistake about the x1,x2=randomize() saying that it cannot unpack non-literable NoneType object, and I don't really know how to fix this.

--- discriminant.py
+++ discriminant.py
@@ -2,6 +2,7 @@ from tkinter import *
 import random
 app=Tk()
 app.geometry("500x500")
+app['bg']="orange"
 # a=5
 # print(f'{"DONKEY" if a>0 else "SHREK"}')
 #
@@ -10,12 +11,16 @@ def discriminant():
     discriminants=b ** 2 - 4 * a * c
     global monkey
     global king_kong
-    monkey['text']=("D=b^2-4ac=" #dont touch
+    
+    monkey['text']=("D=b^2-4ac="#dont touch
+                    
                     f"{b**2}{f'+{a*c*-4}' if a*c*-4>0 else a*c*-4 }={discriminants}\n" # change this
                             "x1=(-b+sqrt(d))/2a\n" #dont touch sqrt is d**(1/2)
                             f"x1={(-b+discriminants**(1/2))/2*a if discriminants>0 else "No more doing that"}\n"# change this
                             "x2=(-b -sqrt(d))/2a\n" #dont touch
                             f"x2={(-b-discriminants**(1/2))/2 if discriminants>0 else "No more doing that"}")# change this return calculating if discriminants>0 also 18 row also change the + - thingy
+
+
     monkey.pack()
     explain['text']=''
     king_kong['text']=''
@@ -23,9 +28,6 @@ def discriminant():
 explain=None
 king_kong=None
 monkey=None
-a=1
-b=6
-c=5
 
 def randomizer():
     for x1 in range (-100,100):
@@ -37,19 +39,36 @@ def discriminantiv2():
     global explain
     global monkey
     global king_kong
-    x1,x2=randomizer()
-    monkey['text'] = (f"x1*x2=c\n"#dont touch
-                      f"x1+x2=-b\n"#dont touch
-                      f"x1*x2={c}\n"# change -5
-                      f"x1+x2={-b}\n"# change -4
-                      f"x1={x1} x2={x2}\n") # change this
-    explain['text']=''
+    x1, x2 = randomizer()
+    monkey['text'] = (f"x1*x2=c\n"  # dont touch
+                      f"x1+x2=-b\n"  # dont touch
+                      f"x1*x2={c}\n"  # change -5
+                      f"x1+x2={-b}\n"  # change -4
+                      f"x1={x1} x2={x2}\n")  # change this
+    explain['text'] =''
 
+# def only_digits(char):
+#     return char.isdigit()
+#
+# vcmd = (app.register(only_digits), '%S')
+# validate="key", validatecommand=vcmd
+
+name=Entry(bg="blue")
+name.pack()
+
+number = Entry(bg="green")
+number.pack()
+
+donkey = Entry(bg="yellow")
+donkey.pack()
+
+# color everything nicely
 def theory():
     global explain,monkey,king_kong
     app.destroy()
     app1=Tk()
     app1.geometry("500x500")
+    app1['bg']='yellow'
     group = StringVar(value =0)
     king_kong=Label(text='')
 
@@ -60,33 +79,48 @@ def theory():
     if b==1 or b==-1:
         b_text = b_text.replace("1", "")
 
-    squared=Label(text=f'{a_text}x^2{f"+{b_text}" if b>0 else b_text}x{f"+{c}"if c>0 else c}=0')
+    squared=Label(text=f'{a_text}x^2{f"+{b_text}" if b>0 else b_text}x{f"+{c}"if c>0 else c}=0',bg="green")
     squared.pack()
 
-    vieta_d=Radiobutton(command=discriminant,text="Discriminant",variable=group,value=1)
+    vieta_d=Radiobutton(command=discriminant,text="Discriminant",variable=group,value=1,bg="red")
     vieta_d.pack()
 
-    vieta_t=Radiobutton(command=discriminantiv2,text="viat",variable=group,value=2)
+    vieta_t=Radiobutton(command=discriminantiv2,text="viat",variable=group,value=2,bg="blue")
     vieta_t.pack()
 
     monkey=Label(text='')
     monkey.pack()
-    explain=Label(text="These 2 choices are the variants of how you can calculate the square root.")
+    explain=Label(text="These 2 choices are the variants of how you can calculate the square root.",bg="aqua")
     explain.place(x=50,y=250)
     app.mainloop()
 
-def sign():
-    name=Entry()
-    name.pack()
+def start():
+    global a,b,c
+    a = (name.get())
+    b = (number.get())
+    c = (donkey.get())
 
-    number = Entry()
-    number.pack()
+    if a.isalpha():
+        good=Label(text="You have to put something other than letters",bg="red")
+        good.place(x=130,y=150)
+        return
 
-    donkey = Entry()
-    donkey.pack()
+    elif b.isalpha():
+        good=Label(text="You have to put something other than letters",bg="red")
+        good.place(x=130,y=150)
+        return
 
-    equation=Button(command=theory,text="Lets start")
-    equation.place(x=210,y=220)
+    elif c.isalpha():
+        good=Label(text="You have to put something other than letters",bg="red")
+        good.place(x=130,y=150)
+        return
 
-sign()
+    a=int(a)
+    b=int(b)
+    c=int(c)
+    theory()
+    
+equation=Button(command=start,text="Lets start",bg="purple")
+equation.place(x=210,y=220)
+
 app.mainloop()
